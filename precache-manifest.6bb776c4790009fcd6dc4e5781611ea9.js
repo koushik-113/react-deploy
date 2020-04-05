d20701123442d7aa02d494e9ab80a9dce51e7bd5
koushik-113 <b.koushik222@gmail.com>
2020-04-05 22:15:51 +0530

    Updates

---
+++ precache-manifest.6bb776c4790009fcd6dc4e5781611ea9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a74e4a5fd7e39fa75a53f54153997b",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "493e5f83750ec7401903",
+    "url": "/react-deploy/static/css/2.19b7558e.chunk.css"
+  },
+  {
+    "revision": "6a188c82a0d61894cacd",
+    "url": "/react-deploy/static/css/main.52286b63.chunk.css"
+  },
+  {
+    "revision": "493e5f83750ec7401903",
+    "url": "/react-deploy/static/js/2.7b3fe2b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-deploy/static/js/2.7b3fe2b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a188c82a0d61894cacd",
+    "url": "/react-deploy/static/js/main.f9d006ad.chunk.js"
+  },
+  {
+    "revision": "0beb5cab4207adf69f81",
+    "url": "/react-deploy/static/js/runtime-main.bec7f21e.js"
+  }
+]);
